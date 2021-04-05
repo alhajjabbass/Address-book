db80996baeecad4706b378e879b3bed23aa3dc35
Abbass Al Hajj <alhajjabbass@gmail.com>
2021-04-05 10:09:37 +0300

    Add unit test project with 1 unit test

---
+++ anghamiApiUnitTests/PeopleServiceTestClass.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace anghamiApiUnitTests
+{
+    [TestClass]
+    public class PeopleServiceTestClass
+    {
+        private readonly PeopleService peopleService;
+
+        public PeopleServiceTestClass(PeopleService service)
+        {
+            peopleService = service;
+        }
+
+        [TestMethod]
+        public void Asserts_GetAllPeople_Returns_List()
+        {
+            //Arrange
+            int page = 1;
+
+            //Act
+            var response = peopleService.GetAllPeople(page);
+
+            //Assert
+            Assert.IsInstanceOfType(response, typeof(List<Person>));
+        }
+    }
+}
---
+++ anghamiApiUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("anghamiApiUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("anghamiApiUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a55e3a77-ec1f-4457-8941-cb3432786144")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
